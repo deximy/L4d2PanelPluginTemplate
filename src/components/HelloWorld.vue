ca75e5d41a159817538b44552da4ae5a0044e78d
deximy <deximy@gmail.com>
2023-02-09 20:57:18 +0800

    feat: initialize a Vue.js 3 template project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+    <div class="card">
+        <h1>{{ msg }}</h1>
+
+        <button type="button" @click="count++">count is {{ count }}</button>
+        <p>
+            Edit
+            <code>components/HelloWorld.vue</code> to test HMR
+        </p>
+
+        <p>
+            Check out
+            <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+            >create-vue</a
+            >, the official Vue + Vite starter
+        </p>
+        <p>
+            Install
+            <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+            in your IDE for a better DX
+        </p>
+        <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    padding: 2em;
+
+    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    line-height: 1.5;
+    font-weight: 400;
+
+    color-scheme: light dark;
+    color: #213547;
+    background-color: #ffffff;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+
+    text-align: center;
+}
+
+h1 {
+    font-size: 3.2em;
+    line-height: 1.1;
+}
+
+button {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: #f9f9f9;
+    cursor: pointer;
+    transition: border-color 0.25s;
+}
+
+button:hover {
+    border-color: #646cff;
+}
+
+button:focus,
+button:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
+}
+
+a {
+    font-weight: 500;
+    color: #66ccff;
+    text-decoration: inherit;
+}
+
+a:hover {
+    color: #646cff;
+}
+
+.read-the-docs {
+    color: #888;
+}
+</style>
